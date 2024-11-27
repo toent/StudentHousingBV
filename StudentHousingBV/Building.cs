87a1d5385ac15172d28d09de7f21a61170ee51ae
Mats <toenterman@gmail.com>
2024-11-27 14:56:25 +0100

    Started on the Datamanager Class Created a Building Class Skeleton Created the storage/data.json directory

---
+++ StudentHousingBV/Building.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentHousingBV
+{
+    public class Building
+    {
+        private DataManager dataManager;
+        private int id;
+        private string address;
+        //private List<Flat> flats;
+        //private List<Rule> buildingRules;
+
+        public Building(string inputAddress)
+        {
+            this.address = inputAddress;
+            //this.flats = new List<Flat>();
+            //this.buildingRules = new List<Rule>();
+            this.id = dataManager.GetIdFromClass(this) + 1;
+        }
+
+        /*public Building(string inputAddress, List<Flat> inputFlats)
+        {
+            this.address = inputAddress;
+            this.flats = inputFlats;
+            this.buildingRules = new List<Rule>();
+            this.id = dataManager.GetIdFromClass(this) + 1;
+        }*/
+
+        /*public Building(string inputAddress, List<Flat> inputFlats, List<Rule> inputBuildingRules)
+        {
+            this.address = inputAddress;
+            this.flats = inputFlats;
+            this.buildingRules = inputBuildingRules;
+            this.id = dataManager.GetIdFromClass(this) + 1;
+        }*/
+
+        public int GetId()
+        { return id; }
+    }
+}
---
+++ StudentHousingBV/DataManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace StudentHousingBV
+{
+    public class DataManager
+    {
+        private List<Building> buildings;
+
+        public DataManager() 
+        {
+            this.buildings = new List<Building>();
+        }
+
+        public int GetIdFromClass(Building building)
+        {
+            int resultId = -1;
+
+            if (buildings.Count > 0)
+            {
+                foreach (Building b in buildings)
+                {
+                    if (b.GetId() > resultId)
+                    {
+                        resultId = b.GetId();
+                    }
+                }
+            }
+
+            return resultId;
+        }
+
+        public List<Building> GetBuildings() 
+        { return buildings; }
+
+        public void SetBuildings(List<Building> newBuildings)
+        { buildings = newBuildings; }
+
+
+        public void LoadFromStorage()
+        {
+            //save json as string
+            string savedJsonText = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Storage", "data.json"));
+
+            try
+            {
+                //deserialize json to Building list
+                buildings = JsonSerializer.Deserialize<List<Building>>(savedJsonText);
+            }
+            catch (Exception ex)
+            {
+                //catch exception
+            }
+
+        }
+
+        public void SaveToStorage()
+        {    
+            //set storage file path 
+            string storagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Storage");
+
+            //create storage file path if it doesnt exist yet
+            Directory.CreateDirectory(storagePath);
+
+            //convert Buildings to json
+            string jsonBuilding = JsonSerializer.Serialize(this.buildings);
+
+            //write or create json file
+            File.WriteAllText(Path.Combine(storagePath, "data.json"), jsonBuilding);
+        }
+    }
+}
