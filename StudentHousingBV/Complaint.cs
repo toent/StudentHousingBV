35d71be8c4172b08c1c0e93f3c6646b973ad11b4
peaceoutommy <tomas.29.lopes@gmail.com>
2024-11-27 14:10:35 +0100

    Complaint Class Structure

--- StudentHousingBV/Complaint.cs
+++ StudentHousingBV/Complaint.cs
@@ -8,5 +8,26 @@ namespace StudentHousingBV
 {
     public class Complaint
     {
+        private int id;
+        private string description;
+        private bool read;
+
+        public int ID { get; }
+        public string Description { get; }
+        public bool Read { get; set; }
+
+        public Complaint(string description)
+        {
+            this.description = description;
+            this.read = false;
+            this.id = generateId();
+        }
+
+        static int generateId()
+        {
+            int id = 0;
+            id++;
+            return id;
+        }
     }
 }
