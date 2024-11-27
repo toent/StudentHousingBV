2c83ba2a00689fc0955a119357ccee3d7406b101
peaceoutommy <tomas.29.lopes@gmail.com>
2024-11-27 13:48:29 +0100

    Complaint class creation

--- StudentHousingBV/obj/Debug/net8.0-windows/StudentHousingBV.AssemblyInfo.cs
+++ StudentHousingBV/obj/Debug/net8.0-windows/StudentHousingBV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentHousingBV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9b009e4bfcbd82c4cc7d2e7c3beb204f35a18f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4c5752c1c4e210e0c9d867c12aaf8ab6ca4de3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentHousingBV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentHousingBV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
