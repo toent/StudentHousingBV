ada457ce538ec1a87c6ba52cbc1eae265bca2c1a
Aleix <aleixurbanoroura@gmail.com>
2024-11-28 09:08:47 +0100

    First design

--- StudentHousingBV/Form1.Designer.cs
+++ StudentHousingBV/Form1.Designer.cs
@@ -28,12 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pSideMenu = new Panel();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            pFormTitle = new Panel();
+            pShowForm = new Panel();
+            button7 = new Button();
+            lblUserIdName = new Label();
+            label1 = new Label();
+            pSideMenu.SuspendLayout();
+            pFormTitle.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pSideMenu
+            // 
+            pSideMenu.Controls.Add(button6);
+            pSideMenu.Controls.Add(button5);
+            pSideMenu.Controls.Add(button4);
+            pSideMenu.Controls.Add(button3);
+            pSideMenu.Controls.Add(button2);
+            pSideMenu.Controls.Add(button1);
+            pSideMenu.Dock = DockStyle.Left;
+            pSideMenu.Location = new Point(0, 0);
+            pSideMenu.Name = "pSideMenu";
+            pSideMenu.Size = new Size(300, 1024);
+            pSideMenu.TabIndex = 0;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(214, 223, 255);
+            button6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.ForeColor = Color.FromArgb(26, 37, 47);
+            button6.Location = new Point(3, 818);
+            button6.Margin = new Padding(3, 20, 3, 20);
+            button6.Name = "button6";
+            button6.Size = new Size(294, 69);
+            button6.TabIndex = 5;
+            button6.Text = "File complaint";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(214, 223, 255);
+            button5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = Color.FromArgb(26, 37, 47);
+            button5.Location = new Point(3, 709);
+            button5.Margin = new Padding(3, 20, 3, 20);
+            button5.Name = "button5";
+            button5.Size = new Size(294, 69);
+            button5.TabIndex = 4;
+            button5.Text = "Groceries";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(214, 223, 255);
+            button4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.FromArgb(26, 37, 47);
+            button4.Location = new Point(3, 600);
+            button4.Margin = new Padding(3, 20, 3, 20);
+            button4.Name = "button4";
+            button4.Size = new Size(294, 69);
+            button4.TabIndex = 3;
+            button4.Text = "Chores";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(214, 223, 255);
+            button3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.FromArgb(26, 37, 47);
+            button3.Location = new Point(3, 491);
+            button3.Margin = new Padding(3, 20, 3, 20);
+            button3.Name = "button3";
+            button3.Size = new Size(294, 69);
+            button3.TabIndex = 2;
+            button3.Text = "Agreements";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(214, 223, 255);
+            button2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.FromArgb(26, 37, 47);
+            button2.Location = new Point(3, 382);
+            button2.Margin = new Padding(3, 20, 3, 20);
+            button2.Name = "button2";
+            button2.Size = new Size(294, 69);
+            button2.TabIndex = 1;
+            button2.Text = "House rules";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(214, 223, 255);
+            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.FromArgb(26, 37, 47);
+            button1.Location = new Point(3, 273);
+            button1.Margin = new Padding(3, 20, 3, 20);
+            button1.Name = "button1";
+            button1.Size = new Size(294, 69);
+            button1.TabIndex = 0;
+            button1.Text = "Announcements";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // pFormTitle
+            // 
+            pFormTitle.Controls.Add(label1);
+            pFormTitle.Controls.Add(lblUserIdName);
+            pFormTitle.Controls.Add(button7);
+            pFormTitle.Dock = DockStyle.Top;
+            pFormTitle.Location = new Point(300, 0);
+            pFormTitle.Name = "pFormTitle";
+            pFormTitle.Size = new Size(1598, 150);
+            pFormTitle.TabIndex = 1;
+            // 
+            // pShowForm
+            // 
+            pShowForm.Dock = DockStyle.Fill;
+            pShowForm.Location = new Point(300, 150);
+            pShowForm.Name = "pShowForm";
+            pShowForm.Size = new Size(1598, 874);
+            pShowForm.TabIndex = 2;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            button7.Location = new Point(1418, 12);
+            button7.Name = "button7";
+            button7.Size = new Size(168, 43);
+            button7.TabIndex = 0;
+            button7.Text = "Logout";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // lblUserIdName
+            // 
+            lblUserIdName.AutoSize = true;
+            lblUserIdName.Location = new Point(1275, 23);
+            lblUserIdName.Name = "lblUserIdName";
+            lblUserIdName.Size = new Size(113, 25);
+            lblUserIdName.TabIndex = 1;
+            lblUserIdName.Text = "Aleix Urbano";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1144, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1898, 1024);
+            Controls.Add(pShowForm);
+            Controls.Add(pFormTitle);
+            Controls.Add(pSideMenu);
+            Name = "Form1";
+            Text = "Form1";
+            pSideMenu.ResumeLayout(false);
+            pFormTitle.ResumeLayout(false);
+            pFormTitle.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel pSideMenu;
+        private Panel pFormTitle;
+        private Panel pShowForm;
+        private Button button1;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Label label1;
+        private Label lblUserIdName;
+        private Button button7;
     }
 }
