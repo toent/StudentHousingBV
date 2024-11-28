22197ed8b17104175c6b1bbd53112f03f486780f
peaceoutommy <tomas.29.lopes@gmail.com>
2024-11-28 11:37:12 +0100

    ComplaintChanges

---
+++ StudentHousingBV/Student App/StudentComplaint.cs
@@ -0,0 +1,31 @@
+﻿using StudentHousingBV.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentHousingBV.Student_App
+{
+    public partial class StudentComplaint : Form
+    {
+        public StudentComplaint()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmitComplaint_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Complaint submitted successfully!");
+        }
+
+        private void changeSelectedTabBgColor()
+        {
+
+        }
+    }
+}
