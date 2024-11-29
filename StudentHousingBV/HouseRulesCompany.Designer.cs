a17329dd021b54e123500b3eb65944a2f3b2a2db
Realgi022 <realgihila@gmail.com>
2024-11-29 13:00:49 +0100

    HouseRulesCompany side done by Seyf

---
+++ StudentHousingBV/HouseRulesCompany.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace StudentHousingBV
+{
+    partial class HouseRulesCompany
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            cbFlat = new ComboBox();
+            panelRules = new Panel();
+            btnCreate = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(485, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(379, 81);
+            label1.TabIndex = 0;
+            label1.Text = "House Rules";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(372, 146);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 27);
+            label2.TabIndex = 1;
+            label2.Text = "Flat";
+            // 
+            // cbFlat
+            // 
+            cbFlat.FormattingEnabled = true;
+            cbFlat.Location = new Point(422, 149);
+            cbFlat.Name = "cbFlat";
+            cbFlat.Size = new Size(151, 28);
+            cbFlat.TabIndex = 2;
+            // 
+            // panelRules
+            // 
+            panelRules.Location = new Point(372, 227);
+            panelRules.Name = "panelRules";
+            panelRules.Size = new Size(863, 157);
+            panelRules.TabIndex = 3;
+            // 
+            // btnCreate
+            // 
+            btnCreate.BackColor = SystemColors.ActiveCaption;
+            btnCreate.Location = new Point(794, 149);
+            btnCreate.Name = "btnCreate";
+            btnCreate.Size = new Size(170, 29);
+            btnCreate.TabIndex = 4;
+            btnCreate.Text = "Create";
+            btnCreate.UseVisualStyleBackColor = false;
+            // 
+            // HouseRulesCompany
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1902, 1033);
+            Controls.Add(btnCreate);
+            Controls.Add(panelRules);
+            Controls.Add(cbFlat);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "HouseRulesCompany";
+            Text = "HouseRulesCompany";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox cbFlat;
+        private Panel panelRules;
+        private Button btnCreate;
+    }
+}
---
+++ StudentHousingBV/HouseRulesCompany.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StudentHousingBV
+{
+    public partial class HouseRulesCompany : Form
+    {
+        private int ruleCount = 0;
+        private List<Control> ruleControls = new List<Control>();
+
+        public HouseRulesCompany()
+        {
+            InitializeComponent();
+            panelRules.AutoScroll = true;
+            panelRules.BorderStyle = BorderStyle.FixedSingle;
+            btnCreate.Click += (s, e) => CreateRule();
+        }
+
+        private void CreateRule()
+        {
+            ruleCount++;
+            var lblRule = CreateLabel();
+            var btnPanel = CreateButtonPanel(lblRule);
+
+            panelRules.Controls.Add(lblRule);
+            panelRules.Controls.Add(btnPanel);
+
+            ruleControls.Add(lblRule);
+            ruleControls.Add(btnPanel);
+
+            ReorganizeRules();
+        }
+
+        private Label CreateLabel()
+        {
+            return new Label
+            {
+                Text = $"Rule {ruleCount}: Input and modify your rules",
+                AutoSize = true,
+                Location = new Point(10, 10 + ruleCount * 60)
+            };
+        }
+
+        private Panel CreateButtonPanel(Label lblRule)
+        {
+            var btnPanel = new Panel { Location = new Point(lblRule.Right + 10, lblRule.Top), Size = new Size(160, 30) };
+
+            var btnModify = new Button { Text = "Modify", Size = new Size(75, 30) };
+            btnModify.Click += (s, e) => ModifyRule(lblRule);
+            btnPanel.Controls.Add(btnModify);
+
+            var btnRemove = new Button { Text = "Remove", Size = new Size(75, 30) };
+            btnRemove.Click += (s, e) => RemoveRule(lblRule, btnPanel);
+            btnPanel.Controls.Add(btnRemove);
+
+            btnModify.Location = new Point(0, 0);
+            btnRemove.Location = new Point(btnModify.Width, 0);
+
+            return btnPanel;
+        }
+
+        private void ModifyRule(Label lblRule)
+        {
+            string input = Prompt.ShowDialog("Modify Rule:", lblRule.Text);
+            if (!string.IsNullOrEmpty(input)) lblRule.Text = input;
+            ReorganizeRules();
+        }
+
+        private void RemoveRule(Label lblRule, Panel btnPanel)
+        {
+            panelRules.Controls.Remove(lblRule);
+            panelRules.Controls.Remove(btnPanel);
+            ruleControls.Remove(lblRule);
+            ruleControls.Remove(btnPanel);
+            ReorganizeRules();
+        }
+
+        private void ReorganizeRules()
+        {
+            int yPos = 10;
+            foreach (Control ctrl in ruleControls)
+            {
+                ctrl.Location = new Point(10, yPos);
+                yPos += ctrl.Height + 10;
+            }
+            panelRules.AutoScrollMinSize = new Size(panelRules.Width, yPos);
+        }
+    }
+
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            var prompt = new Form
+            {
+                Width = 300,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            var textBox = new TextBox { Left = 10, Top = 50, Width = 260 };
+            var confirmation = new Button { Text = "OK", Left = 200, Width = 70, Top = 80, DialogResult = DialogResult.OK };
+
+            confirmation.Click += (s, e) => prompt.Close();
+
+            prompt.Controls.Add(new Label { Left = 10, Top = 20, Text = text, Width = 260 });
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+    }
+}
