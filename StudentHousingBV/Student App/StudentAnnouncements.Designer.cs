2a45df9d81cc407d1efd4200c69c3713a10c8704
Realgi022 <realgihila@gmail.com>
2024-12-13 13:28:03 +0100

    Changed the annoucment page made the scrolling avilable if there are a lot

---
+++ StudentHousingBV/Student App/StudentAnnouncements.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace StudentHousingBV.Student_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(277, 90);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1160, 565);
+            panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+    }
+}
---
+++ StudentHousingBV/Student App/StudentAnnouncements.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace StudentHousingBV.Student_App
+{
+    public partial class StudentAnnouncements : Form
+    {
+        public StudentAnnouncements()
+        {
+            InitializeComponent();
+            LoadAnnouncements();
+        }
+
+        private void LoadAnnouncements()
+        {
+            var announcements = new[]
+            {
+        new { Title = "New Library Hours", Timestamp = DateTime.Now.AddHours(-3), Content = "Library hours extended to 10 PM starting next week." },
+        new { Title = "Exam Schedule Update", Timestamp = DateTime.Now.AddDays(-1), Content = "Final exams will begin on January 2nd. Check the portal for details." },
+        new { Title = "Holiday Event", Timestamp = DateTime.Now.AddDays(-5), Content = "Join us for a holiday event in the main auditorium on December 20th!" },
+        new { Title = "Maintenance Notice", Timestamp = DateTime.Now.AddHours(-10), Content = "The dormitory water supply will be off on December 15th from 10 AM to 2 PM." },
+        new { Title = "Fire Drill", Timestamp = DateTime.Now.AddDays(-2), Content = "There will be a mandatory fire drill on December 16th at 3 PM. Attendance is required." },
+        new { Title = "Fire Drill", Timestamp = DateTime.Now.AddDays(-2), Content = "There will be a mandatory fire drill on December 16th at 3 PM. Attendance is required." },
+        new { Title = "Fire Drill", Timestamp = DateTime.Now.AddDays(-2), Content = "There will be a mandatory fire drill on December 16th at 3 PM. Attendance is required." },
+        new { Title = "Fire Drill", Timestamp = DateTime.Now.AddDays(-2), Content = "There will be a mandatory fire drill on December 16th at 3 PM. Attendance is required." },
+        new { Title = "Fire Drill", Timestamp = DateTime.Now.AddDays(-2), Content = "There will be a mandatory fire drill on December 16th at 3 PM. Attendance is required." },
+        new { Title = "Fire Drill", Timestamp = DateTime.Now.AddDays(-2), Content = "There will be a mandatory fire drill on December 16th at 3 PM. Attendance is required." },
+
+    };
+
+            int yOffset = 10; 
+
+            foreach (var announcement in announcements)
+            {
+                var announcementPanel = new Panel
+                {
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Location = new Point(10, yOffset),
+                    Width = panelAnnoucmentControls.ClientSize.Width - 20,
+                    AutoSize = true
+                };
+
+                var titleLabel = new Label
+                {
+                    Text = announcement.Title,
+                    Font = new Font("Arial", 12, FontStyle.Bold),
+                    AutoSize = true,
+                    Location = new Point(5, 5)
+                };
+                announcementPanel.Controls.Add(titleLabel);
+
+                var timestampLabel = new Label
+                {
+                    Text = announcement.Timestamp.ToString("g"),
+                    Font = new Font("Arial", 9, FontStyle.Italic),
+                    ForeColor = Color.Gray,
+                    AutoSize = true,
+                    Location = new Point(5, titleLabel.Bottom + 5)
+                };
+                announcementPanel.Controls.Add(timestampLabel);
+
+                var contentLabel = new Label
+                {
+                    Text = announcement.Content,
+                    Font = new Font("Arial", 10),
+                    AutoSize = true,
+                    Location = new Point(5, timestampLabel.Bottom + 5)
+                };
+                announcementPanel.Controls.Add(contentLabel);
+
+                panelAnnoucmentControls.Controls.Add(announcementPanel);
+
+                yOffset += announcementPanel.Height + 10;
+            }
+
+            panelAnnoucmentControls.AutoScrollMinSize = new Size(0, yOffset);
+        }
+
+        private void InitializeComponent()
+        {
+            panelAnnoucmentControls = new Panel();
+            SuspendLayout();
+            panelAnnoucmentControls.AutoScroll = true;
+            panelAnnoucmentControls.Location = new Point(10, 44);
+            panelAnnoucmentControls.Margin = new Padding(2);
+            panelAnnoucmentControls.Name = "panelAnnoucmentControls";
+            panelAnnoucmentControls.Size = new Size(1259, 611);
+            panelAnnoucmentControls.TabIndex = 3;
+
+            ClientSize = new Size(1278, 699);
+            Controls.Add(panelAnnoucmentControls);
+            Name = "StudentAnnouncements";
+            ResumeLayout(false);
+        }
+
+        private Panel panelAnnoucmentControls;
+
+    }
+}
