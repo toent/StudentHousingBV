a245d33d48cef3e8dd2fe8334b9683e51b5d678b
Aleix Urbano Roura <39736542+aleixurbano@users.noreply.github.com>
2025-01-23 17:51:18 +0100

    Add program.cs

---
+++ StudentHousingBV/Program.cs
@@ -0,0 +1,20 @@
+﻿using StudentHousingBV.Company_App;
+using StudentHousingBV.Student_App;
+
+namespace StudentHousingBV
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new StudentLogin());
+        }
+    }
+}
